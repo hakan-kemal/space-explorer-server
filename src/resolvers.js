b29b232faec8438639de2c6cf12aa3eb0e9eebfe
hakan0814 <h.k.cuhadar@gmail.com>
2019-12-10 22:15:11 +0100

    Setup BE files

---
+++ src/resolvers.js
@@ -0,0 +1,20 @@
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+];
+
+module.exports = resolvers;
