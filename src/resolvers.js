4440ced9dfc99c660873d73536042d1e046f0d20
hakan0814 <h.k.cuhadar@gmail.com>
2019-12-11 01:14:01 +0100

    Setup BE files

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,3 @@
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
     books: () => books
